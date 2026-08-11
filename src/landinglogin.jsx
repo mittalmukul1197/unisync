ca5aceb6a7c268cb417eb3e90621591eb36afb3d
Mukul Mittal <mittalmukul1197@gmail.com>
2026-08-11 20:49:35 +0530

    login

---
+++ src/landinglogin.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './landinglogin.css'
+// import './assets' 
+
+
+
+const card = 
+<div className='logincard'>
+    <div>
+        <img src="../assets/logo.png" alt="" />
+    </div>
+
+</div>
+
+createRoot(document.getElementById('root')).render(
+    card
+
+)
+
