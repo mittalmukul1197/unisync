f6726565dc242461e72beb014c318a2c6167411d
Mukul Mittal <mittalmukul1197@gmail.com>
2026-08-12 20:23:24 +0530

    loginform

--- src/landinglogin.jsx
+++ src/landinglogin.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './landinglogin.css'
-// import './assets' 
+import './landinglogin.css';
+import log from './assets/logo.png';
+import aplh from './assets/stylishname(1).png';
 
-
-
-const card = 
-<div className='logincard'>
+const card = (
+  <div className='logincard'>
     <div>
-        <img src="../assets/logo.png" alt="" />
+      <img src={log} alt="Logo" />
     </div>
+    <div className='form'>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="" id="email" placeholder='Enter your mail' />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="password" name="" id="password" placeholder='Enter your Password' />
+      </div>
+    </div>
+  </div>
+);
 
-</div>
+createRoot(document.getElementById('root')).render(card);
 
-createRoot(document.getElementById('root')).render(
-    card
 
-)
+
 
