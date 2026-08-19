5265b62cd3ec44b857b5d4dd3502d8c21fd65a63
Mukul Mittal <mittalmukul1197@gmail.com>
2026-08-19 11:42:40 +0530

    link

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,21 @@ import { FacultySidebar } from './components/layout/navbar';
 import { StudentHeader } from './components/layout/header';
 import { FacultyHeader } from './components/layout/header';
 import {StudentSidebar} from './components/layout/navbar';
-import Footer from './components/layout/footer';
+// import Footer from './components/layout/footer';
+import { TeacherUpdatesDrawer} from './components/academic/facTeacherUpdates';
+import { TeacherUpdatesWidget } from './components/academic/facTeacherUpdates';
+import TeacherUpdates  from './components/academic/facTeacherUpdates';
 
 function App() {
   return (
     <div className="app-wrapper">
-<StudentHeader/>
-<Footer/> 
+{/* <StudentHeader/>
+<Footer/>  */}
+{/* <TeacherUpdatesDrawer isOpen={true}/> */}
+{/* <TeacherUpdatesWidget isOpen={true}/> */}
 
+
+<TeacherUpdates/>
 
    </div>
   );
